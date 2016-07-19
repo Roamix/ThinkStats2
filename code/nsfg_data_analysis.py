9517e838c8331ffdc505b58612c135d791f62375
Roamix <Christoffer.Hjort1995@gmail.com>
2016-07-19 15:55:47 +0200

    laptop

--- code/nsfg_data_analysis.py
+++ code/nsfg_data_analysis.py
@@ -1,5 +1,6 @@
-import numpy as np
 
+import numpy as np
+import pandas as pd
 import nsfg
 import chap01ex as c1
 import datsys as ds
@@ -29,4 +30,15 @@ laters = live[live.birthord>1]
 
 ##******DO EXCERCISE 2.2 and 2.4*******
 
-#5  Probability mass functions
+#5  Probability mass functions/Probabilty distribution functions
+n1, bins1, typ1 = plt.hist(firsts.prglngth, bins=range(0, int(max(firsts.prglngth))+1), normed=True)
+n2, bins2, typ2 = plt.hist(laters.prglngth, bins=range(0, int(max(laters.prglngth))+1), normed=True)
+#or firsts.prglngth.hist(bins=range(0, int(max(firsts.prglngth))+1), normed=True)
+
+#Calculating and plotting probabiltiy differences between two groups
+weeks = bins1[:-1]
+diffs = [100*(n1[week]-n2[week]) for week in weeks]  #scalar 100 for bigger bars
+plt.bar(weeks[35:47], diffs[35:47])
+plt.show()
+
+#******DO CHAPTER 3 EXCERCISES*******
